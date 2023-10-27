110cd376782c628f99c746562fda37eb3d26bf2f
mdhamid1310 <john@example.com>
2023-10-27 09:41:08 +0530

    Add project files.

---
+++ Section8_1.19/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section8_1._19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+            Console.ReadLine();
+        }
+        public static void runApp()
+        {
+            string[] students = { "Charlie", "Alice", "Bob",  "David", "Ella", "Jack", "Grace", "Frank", "Henry", "Ivy"  };
+
+            Console.WriteLine("Unsorted Students:");
+            DisplayStudents(students);
+
+            BubbleSort(students);
+
+            Console.WriteLine("\nSorted Students:");
+            DisplayStudents(students);
+        }
+
+        public static void BubbleSort(string[] arr)
+        {
+            int n = arr.Length;
+            bool swapped;
+
+            do
+            {
+                swapped = false;
+                for (int i = 1; i < n; i++)
+                {
+                    if (string.Compare(arr[i - 1], arr[i]) > 0)
+                    {
+                        // Swap the elements
+                        string temp = arr[i - 1];
+                        arr[i - 1] = arr[i];
+                        arr[i] = temp;
+                        swapped = true;
+                    }
+                }
+                n--;
+            } while (swapped);
+        }
+
+        public static void DisplayStudents(string[] arr)
+        {
+            foreach (string student in arr)
+            {
+                Console.Write(student + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
